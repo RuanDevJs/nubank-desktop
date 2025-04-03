3199ade417adcee75595b95d6c7a384c564cebc3
RuanDevJs <ruanvelpidio@hotmail.com>
2025-04-03 20:46:08 -0300

    feature: Adicionado card de 'Planeje as suas contas' na página home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Account from "@/components/Home/Account";
-import { CreditCard, DollarSign, EyeClosed, Heart, IdCard } from "lucide-react";
+import { CreditCard, DollarSign, EyeClosed, Heart, IdCard, SquareArrowOutUpRight } from "lucide-react";
 import { Galleria } from "primereact/galleria";
 import { JSX } from "react";
 
@@ -8,7 +8,7 @@ const SLIDE_GALLERY = [
   {
     title: "Convide todo mundo para o Nu:",
     description: "Coisa boa a gente compartilha.",
-    icon: <Heart fill="#B30FE2" size={32} color="#B30FE2" />
+    icon: <Heart fill="#B30FE2" size={28} color="#B30FE2" />
   },
   {
     title: "Você tem até R$ 20.000 disponíveis",
@@ -28,7 +28,7 @@ function Gallery(props: { title: string, description: string, icon: JSX.Element 
       <h2 className="font-normal text-lg 2xl:text-xl text-zinc-100">
         {props.title} <br /> {props.description}
       </h2>
-      <div className="bg-black p-1.5 rounded-full cursor-pointer">
+      <div className="bg-black h-12 w-12 flex items-center justify-center rounded-full cursor-pointer">
         {props.icon}
       </div>
     </div>
@@ -56,8 +56,8 @@ export default function Home() {
             Saiba mais
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-3 min-h-[190px]">
-          <div className="w-full h-full">
+        <div id="cards-home" className="grid grid-cols-3 gap-5 min-h-[190px]">
+          <div className="w-full h-[220px]">
             <p className="grid grid-cols-[1fr_auto] gap-1.5 items-center before:block before:flex-1/2 before:h-[1px] before:bg-zinc-500 text-sm font-normal text-zinc-300">Cartões de crédito</p>
             <div className="bg-[#0F0F0F] h-full mt-2.5 rounded-lg p-5">
               <div className="flex items-start justify-between">
@@ -69,7 +69,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="w-full h-[220px]">
             <p className="grid grid-cols-[1fr_auto] gap-1.5 items-center before:block before:flex-1/2 before:h-[1px] before:bg-zinc-500 text-sm font-normal text-zinc-300">Empréstimos</p>
             <div className="bg-[#0F0F0F] h-full mt-2.5 rounded-lg p-5">
               <div className="flex items-start justify-between">
@@ -81,8 +81,12 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>
-            Fatura atual
+          <div className="w-full h-[250px] rounded-lg p-5 bg-[#4E0578] relative">
+            <h2 className="2xl:text-2xl text-lg font-medium text-zinc-200">Planeje as suas contas</h2>
+            <p className="text-base font-normal text-zinc-300 mt-2">Acompanhe, pague e programe com a ajuda do Assistente de pagamentos.</p>
+            <button className="bg-black h-12 w-12 flex items-center justify-center absolute bottom-0 right-0 rounded-tl-lg cursor-pointer">
+              <SquareArrowOutUpRight size={24} />
+            </button>
           </div>
         </div>
       </div>
--- src/components/Home/Account.tsx
+++ src/components/Home/Account.tsx
@@ -25,23 +25,23 @@ export default function Account() {
         </div>
         <div>
           <p className='text-sm font-normal text-zinc-500 py-1.5'>Saldo na conta</p>
-          <input type={inputInformationType} className='w-full border-none outline-none font-bold text-base text-white' value="R$ 750,00" readOnly />
+          <input type={inputInformationType} className='w-full border-none outline-none font-bold text-base text-white' defaultValue="R$ 750,00" readOnly />
         </div>
         <div className='mt-8 bg-[#4E0578] w-[180px] h-[180px] rounded-full flex items-center justify-center hover:brightness-125 transition ease-in-out cursor-pointer'>
           <div className='bg-[#2C2C2C] w-[130px] h-[130px] border-[10px] border-[#000] rounded-full flex flex-col items-center justify-center'>
             <p className='text-xs'>Total investido: </p>
-            <input type={inputInformationType} className='w-full border-none outline-none font-bold text-base text-white text-center' value="R$ 450,00" readOnly />
+            <input type={inputInformationType} className='w-full border-none outline-none font-bold text-base text-white text-center' defaultValue="R$ 450,00" readOnly />
           </div>
         </div>
         <div className='mt-7'>
           <ul className='space-y-2'>
             <li className='flex justify-between items-center'>
               <p className='flex items-center text-xs font-normal text-white before:block before:w-1.5 before:h-1.5 before:bg-[#B30FE2] before:rounded-full gap-1.5'>Saldo na conta</p>
-              <input type={inputInformationType} className='w-[35%] border-none outline-none font-bold text-base text-white' value="R$ 750,00" />
+              <input type={inputInformationType} className='w-[35%] border-none outline-none font-bold text-base text-white' defaultValue="R$ 750,00" />
             </li>
             <li className='flex justify-between items-center'>
               <p className='flex items-center text-xs font-normal text-white before:block before:w-1.5 before:h-1.5 before:bg-[#2E054B] before:rounded-full gap-1.5'>Saldo de investimento</p>
-              <input type={inputInformationType} className='w-[35%] border-none outline-none font-bold text-base text-white' value="R$ 450,00" />
+              <input type={inputInformationType} className='w-[35%] border-none outline-none font-bold text-base text-white' defaultValue="R$ 450,00" />
             </li>
             <li className='mt-10'>
               <button className='bg-[#B30FE2] w-32 py-1.5 rounded-2xl cursor-pointer hover:brightness-110 transition ease-in-out'>
