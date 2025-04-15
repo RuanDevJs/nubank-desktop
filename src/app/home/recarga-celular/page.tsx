87823658149dd97c3a36080c3d64d030f3a19f68
RuanDevJs <ruanvelpidio@hotmail.com>
2025-04-15 10:39:45 -0300

    feature: finalizado página de transfêrencia

--- src/app/home/recarga-celular/page.tsx
+++ src/app/home/recarga-celular/page.tsx
@@ -14,23 +14,10 @@ const priceVariant = tv({
   }
 })
 
-export default function Transferencia() {
-  const prices = [5, 10, 50, 100, 1000, 10000];
-  const [selectedPrice, setSelectedPrice] = useState<string>("");
-
-  function Price({ price }: { price: number }) {
-    const validate = selectedPrice === `R$ ${price},00`;
-
-    return (
-      <div className={priceVariant({ isSelected: validate })} onClick={() => setSelectedPrice(`R$ ${price},00`)}>
-        <span className='font-normal text-sm text-zinc-300'>R${price}</span>
-      </div>
-    )
-  }
-
+export default function RecargaCelular() {
   return (
     <div className='w-full h-full opacity-0 animate-up'>
-      <div className='bg-[#0F0F0F] h-[65%] w-[100%] px-8 py-10 rounded m-auto'>
+      <div className='bg-[#0F0F0F] h-[auto] w-[100%] px-8 py-10 rounded m-auto'>
         <div className='group flex items-center justify-between'>
           <Link href="/home" className='flex items-center cursor-pointer'>
             <button className='bg-[#2C2C2C] group-hover:brightness-125 w-10 h-10 flex items-center justify-center rounded-full'>
@@ -47,20 +34,13 @@ export default function Transferencia() {
           </div>
         </div>
         <div className='mt-7'>
-          <h1 className='text-2xl font-medium'>Qual é o valor da sua transferência?</h1>
-          <p className='text-base font-medium text-[#C8C2C2] mt-1.5'>Saldo disponível de R$ 38.500,00</p>
+          <h1 className='text-2xl font-medium'>Qual número você quer recarregar?</h1>
           <input
             type="text"
             className='block w-full outline-0 border-0 border-b border-zinc-500 font-normal text-xl mt-7 px-1 text-zinc-200'
-            placeholder='R$ 0'
+            placeholder='(00) 00000-0000'
             inputMode='numeric'
-            value={selectedPrice}
-            onChange={({ target }) => setSelectedPrice(target.value)}
-            onBlur={(event) => selectedPrice.length && !selectedPrice.includes("R$") ? setSelectedPrice(`R$ ${event.target.value},00`) : null}
           />
-        </div>
-        <div className='flex items-center gap-3 mt-7'>
-          {prices.map((price) => <Price price={price} key={price} />)}
         </div>
         <div className="mt-7">
           <p className='flex gap-1.5 items-center text-[#A30CFF] text-base font-normal border-b border-[#A30CFF] w-fit cursor-pointer hover:brightness-125 transition ease-in-out'>
@@ -71,7 +51,7 @@ export default function Transferencia() {
         <div className='flex items-center w-full mt-12 group'>
           <div className='flex items-center w-full'>
             <button className='bg-transparent border border-[#2c2c2c] w-[94%] rounded-full h-10 px-3.5 font-normal text-base cursor-pointer transition ease-in-out group-hover:bg-[#a30cff] duration-[0.32s]'>
-              Transferir
+              Pagar recarga
             </button>
             <button className='bg-transparent border border-[#2c2c2c] w-[6%]  h-10 flex items-center justify-center rounded-full cursor-pointer transition ease-in-out group-hover:bg-[#a30cff] duration-[0.32s]'>
               <ArrowRight size={25} />
