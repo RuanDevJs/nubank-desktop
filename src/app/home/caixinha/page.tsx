4ea3ab1b894365def16e435858628be3d81332d6
RuanDevJs <ruanvelpidio@hotmail.com>
2025-04-18 14:30:50 -0300

    feature: Finalizado card superior da página de 'caixinha'

--- src/app/home/caixinha/page.tsx
+++ src/app/home/caixinha/page.tsx
@@ -1,11 +1,11 @@
-import { EyeClosed, Wallet } from 'lucide-react'
-import React from 'react'
+import { ArrowRight, EyeClosed, Plus, Wallet } from 'lucide-react'
+import NuLimiteGarantido from "@/assets/Caixinha/NuLimiteGarantido.png"
 
 export default function page() {
   return (
     <div>
-      <div className='grid grid-cols-[240px_1fr] h-[500px] gap-5'>
-        <div className='bg-[#0F0F0F] p-5 rounded flex flex-col justify-between'>
+      <div className='grid grid-cols-[240px_1fr] min-h-[500px] gap-5'>
+        <div className='bg-[#0F0F0F] p-5 rounded flex flex-col justify-between opacity-0 animate-down' style={{ animationDelay: '0.32s' }}>
           <div>
             <div className='flex items-center justify-between'>
               <h1 className='text-lg font-normal'>
@@ -23,29 +23,73 @@ export default function page() {
             Saiba mais
           </button>
         </div>
-        <div className='h-full'>
-          <h2 className='grid grid-cols-[auto_1fr] gap-5 items-center after:block after:w-full after:h-[1px] after:bg-zinc-500 text-sm font-normal text-white'>
+        <div className='h-full opacity-0 animate-up' style={{ animationDelay: '0.42s' }}>
+          <h2 className='h-[28px] grid grid-cols-[auto_1fr] gap-5 items-center after:block after:w-full after:h-[1px] after:bg-zinc-500 text-sm font-normal text-white'>
             <span className='bg-[#2C2C2C] rounded-full py-1 px-3'>
               Minha organização
             </span>
           </h2>
-          <div className='grid grid-cols-3 gap-5 h-[460px] mt-2'>
-            <div className='h-full'>
-              <div className='bg-[#2E054B] rounded h-[82%] 2xl:px-5 2xl:py-8 p-3 flex items-end justify-start'>
+          <div className='grid grid-cols-3 gap-5 h-[310px] mt-2.5 overflow-hidden'>
+            <div className='h-full '>
+              <div className='bg-[#2E054B] h-[80%] rounded-2xl 2xl:px-5 2xl:py-8 p-3 flex items-end justify-start'>
                 <div className='flex flex-col gap-5'>
                   <Wallet size={40} strokeWidth={1.8} color='#BE87FF' className='border-2 border-[#BE87FF] rounded-full p-1' />
                   <h3 className='text-[#BE87FF] text-3xl 2xl:text-5xl font-medium'>Minha Carteira</h3>
                 </div>
               </div>
+              <div className='flex items-center w-full h-[20%]'>
+                <button className='bg-[#2E054B] border border-[#2E054B] w-[80%] 2xl:w-[90%] rounded-full h-10 px-3.5 font-normal text-base cursor-pointer transition ease-in-out group-hover:bg-[#2E054B] duration-[0.32s] group-hover:border-[#2E054B]'>
+                  Transferir
+                </button>
+                <button className='bg-[#2E054B] border border-[#2E054B] w-[20%] 2xl:w-[10%]  h-10 flex items-center justify-center rounded-full cursor-pointer transition ease-in-out group-hover:bg-[#2E054B] group-hover:border-[#2E054B] duration-[0.32s]'>
+                  <ArrowRight size={25} />
+                </button>
+              </div>
             </div>
-            <div className='bg-[#8F0BE6] rounded h-[50%]'>
-              <h3>Minha Carteira</h3>
+            <div className='h-full'>
+              <img src={NuLimiteGarantido.src} alt="" className='h-[250px] w-full' />
+              <div className='flex flex-col justify-center w-full h-[20%]'>
+                <p className='text-base text-zinc-500 font-normal'>Nu Limite Garantido</p>
+                <p className='text-base text-white font-medium'>R$ 000.000,00</p>
+              </div>
             </div>
-            <div className='bg-[#8F0BE6] rounded h-[50%]'>
-              <h3>Minha Carteira</h3>
+            <div className='h-full'>
+              <div className='bg-[#002247] rounded-2xl h-[80%] flex flex-col items-center justify-center'>
+                <Plus size={64} color='#25799E' />
+              </div>
+              <div className='h-[20%] flex items-start'>
+                <p className='mt-3 text-zinc-100 font-normal'>Criar caixinha</p>
+              </div>
+            </div>
+          </div>
+          <div className='mt-3 h-[154px]'>
+            <h2 className='grid grid-cols-[1fr_auto] gap-5 items-center before:block before:w-full before:h-[1px] before:bg-zinc-500 text-sm font-normal text-white h-[20%]'>
+              <span className='font-normal text-base text-zinc-200'>
+                Cartões de crédito
+              </span>
+            </h2>
+            <div className='h-[80%] bg-[#0F0F0F] rounded-2xl'>
+              <div className='h-full flex flex-col justify-center px-10'>
+                <div className='flex justify-between items-center'>
+                  <div>
+                    <h2 className='text-xl font-normal'>Total em criptomoedas</h2>
+                    <input type='password'
+                      className='w-full border-none outline-none font-bold text-base text-white mt-3.5' defaultValue="R$ 750,00"
+                      readOnly />
+                  </div>
+                  <div className='w-[42px] h-[42px] rounded-full bg-zinc-800 flex items-center justify-center cursor-pointer hover:brightness-125 transition ease-in-out'>
+                    <ArrowRight size={30} color="#fff" />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className='h-[240px] mt-2'>
+        <h2 className='h-[28px] grid grid-cols-[auto_1fr] gap-5 items-center after:block after:w-full after:h-[1px] after:bg-zinc-500 text-sm font-medium text-white'>
+          Seguros
+        </h2>
       </div>
     </div>
   )
