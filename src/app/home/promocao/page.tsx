f7e834d12a57a71000f4028648c3c6fc61cf94c1
RuanDevJs <ruanvelpidio@hotmail.com>
2025-05-15 11:45:35 -0300

    feature: Criado seção de cupons

--- src/app/home/promocao/page.tsx
+++ src/app/home/promocao/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import { ArrowRight } from "lucide-react";
-
 import Cashback from "@/components/Promocao/Cashback";
 import Heading from "@/components/Promocao/Heading";
 import BlackFriday from "@/components/Promocao/BlackFriday";
@@ -8,11 +5,16 @@ import Shopping from "@/components/Promocao/Shopping";
 import Category from "@/components/Promocao/Category";
 import Iphone from "@/components/Promocao/Iphone";
 import Renovation from "@/components/Promocao/Renovation";
+import PortableAppliances from "@/components/Promocao/PortableAppliances";
 
-import LeCreuset from "@/assets/Promocao/Portable Appliances/Le Creuset.jpg";
-import CasasBahia from "@/assets/Promocao/Portable Appliances/Casas Bahia.jpg";
-import Midea from "@/assets/Promocao/Portable Appliances/Midea.jpg";
-import Shopclub from "@/assets/Promocao/Portable Appliances/Shopclub.jpg";
+import Image from "next/image";
+
+import Produtos from "@/assets/Promocao/Cupom/Produtos.png"
+
+import Amazon from "@/assets/Promocao/Cashback/Amazon.svg"
+import Aliexpress from "@/assets/Promocao/Cashback/Aliexpress.svg"
+import MagazineLuiza from "@/assets/Promocao/Cashback/Magazine Luiza.svg"
+import Shopee from "@/assets/Promocao/Cashback/Shopee.svg"
 
 export default function Promocao() {
   return (
@@ -24,63 +26,41 @@ export default function Promocao() {
       <Category />
       <Iphone />
       <Renovation />
-      <div id="portable-appliances" className="w-[99%] mt-7">
-        <div id="portable appliances-heading">
-          <div className="grid grid-cols-[1fr_auto] gap-3.5 items-center">
-            <h3 className="grid gap-3 grid-cols-[auto_1fr] items-center after:block after:w-full after:h-[1px] after:bg-zinc-700 text-sm font-semibold text-white">Confira os eletroportáteis</h3>
-            <div className="flex items-center group">
-              <button className="bg-[#0F0F0F] border border-[#909090] py-1.5 px-3 rounded-full group-hover:brightness-125 cursor-pointer transition ease-in-out ">Mostrar mais</button>
-              <button className="bg-[#0F0F0F] border border-[#909090] p-1 rounded-full group-hover:brightness-125 cursor-pointer transition ease-in-out">
-                <ArrowRight size={25} />
-              </button>
-            </div>
-          </div>
-          <p className="font-light text-2xl text-zinc-100">nas lojas parceiras do Shopping do Nu.</p>
+      <PortableAppliances />
+      <div id="cupons" className="mt-6 w-[99%]">
+        <div>
+          <h3 className="grid grid-cols-[auto_1fr] gap-3 items-center after:block after:w-full after:h-[1px] after:bg-zinc-700 text-sm font-semibold text-white">Cupons exclusivos</h3>
+          <p className="font-light text-2xl text-zinc-100 mt-2.5">Com desconto nas suas lojas favoritas.</p>
         </div>
-        <div className="grid grid-cols-2 items-start gap-5 mt-5">
-          <div className="flex items-center gap-5">
-            <div>
-              <Image src={LeCreuset.src} width={LeCreuset.width} height={LeCreuset.height} alt="" className="rounded-lg object-cover cursor-pointer" draggable={false} />
+        <div className="bg-[#ECECEC] max-w-[70%] 2xl:max-w-[50%] h-[200px] mx-auto mt-5 rounded">
+          <div className="grid grid-cols-[250px_1fr] gap-10 h-full items-center">
+            <div className="bg-[#B30FE2] rounded h-full p-5 relative">
+              <div>
+                <Image src={Produtos.src} width={Produtos.width} height={Produtos.height} quality={80} className="object-cover w-full h-full" alt="" />
+              </div>
+              <div className="w-[30px] h-[30px] bg-black rounded-full absolute top-[-10px] right-[50px]" />
+              <div className="w-[30px] h-[30px] bg-black rounded-full absolute bottom-[-10px] right-[50px]" />
             </div>
-            <div>
-              <p className="text-sm text-zinc-500 font-light">Le Creuset</p>
-              <h2 className="text-xl text-zinc-100 font-medium mt-1">3,5% de <br /> cashback</h2>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div>
-              <Image src={CasasBahia.src} width={CasasBahia.width} height={CasasBahia.height} alt="" className="rounded-lg object-cover cursor-pointer" draggable={false} />
-            </div>
-            <div>
-              <p className="text-sm text-zinc-500 font-light">Casas Bahia</p>
-              <h2 className="text-xl text-zinc-100 font-medium mt-1">5% de <br /> cashback</h2>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div>
-              <Image src={Midea.src} width={Midea.width} height={Midea.height} alt="" className="rounded-lg object-cover cursor-pointer" draggable={false} />
-            </div>
-            <div>
-              <p className="text-sm text-zinc-500 font-light">Midea</p>
-              <h2 className="text-xl text-zinc-100 font-medium mt-1">1,5% de <br /> cashback</h2>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div>
-              <Image src={Shopclub.src} width={Shopclub.width} height={Shopclub.height} alt="" className="rounded-lg object-cover cursor-pointer" draggable={false} />
-            </div>
-            <div>
-              <p className="text-sm text-zinc-500 font-light">ShopClub</p>
-              <h2 className="text-xl text-zinc-100 font-medium mt-1">1% de <br /> cashback</h2>
+            <div className="w-full">
+              <div className="mb-3.5">
+                <h2 className="text-2xl text-zinc-950 font-medium">Descontos incríveis e <br /> cupons exclusivos.</h2>
+              </div>
+              <div className="flex gap-3">
+                <div>
+                  <Image src={Amazon} width={50} height={50} quality={80} className="object-cover" alt="" />
+                </div>
+                <div>
+                  <Image src={Aliexpress} width={50} height={50} quality={80} className="object-cover" alt="" />
+                </div>
+                <div>
+                  <Image src={MagazineLuiza} width={50} height={50} quality={80} className="object-cover" alt="" />
+                </div>
+                <div>
+                  <Image src={Shopee} width={50} height={50} quality={80} className="object-cover" alt="" />
+                </div>
+              </div>
             </div>
           </div>
-
-        </div>
-        <div className="flex items-center justify-center mt-10">
-          <button className="bg-[#0F0F0F] border border-[#909090] py-2 px-5 rounded-full group-hover:brightness-125 cursor-pointer transition ease-in-out text-lg">Ver mais lojas parceiras</button>
-          <button className="bg-[#0F0F0F] border border-[#909090] p-1 rounded-full group-hover:brightness-125 cursor-pointer transition ease-in-out">
-            <ArrowRight size={25} />
-          </button>
         </div>
       </div>
     </div>
